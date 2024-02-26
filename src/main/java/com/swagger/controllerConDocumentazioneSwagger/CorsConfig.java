5d4e764d2a4f1d3a22d5e135068edd3949fbb5b2
Antonio Taminto <taminto.a@gmail.com>
2024-02-26 17:01:39 +0100

    commit

--- src/main/java/com/swagger/controllerConDocumentazioneSwagger/CorsConfig.java
+++ src/main/java/com/swagger/controllerConDocumentazioneSwagger/CorsConfig.java
@@ -1,3 +1,5 @@
+package com.swagger.controllerConDocumentazioneSwagger;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
